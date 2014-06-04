8380a8cdf763d5e6390984f6fb2bd3c10f090394
Stephen Crawley <s.crawley@uq.edu.au>
2014-06-04 10:36:41 +1000

    The 'subsys' lockdir does not exist by default on ubuntu

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,6 @@
+node.default['omero']['home'] = '/opt/omero/OMERO.server'
+node.default['omero']['user'] = 'omero'
+
 node.normal['java']['install_flavor'] = 'openjdk'
 node.normal['java']['jdk_version'] = '7'
 node.normal['java']['accept_license_agreement'] = true
