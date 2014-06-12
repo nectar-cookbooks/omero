582264e6b29b5d33cd2d14b745a229460f759a9b
Stephen Crawley <s.crawley@uq.edu.au>
2014-06-12 10:41:43 +1000

    Omero web (development version)

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,6 +8,10 @@ node.default['omero']['db_password'] = 'db_password'
 node.default['omero']['db_name'] = 'omero_database'
 node.default['omero']['root_password'] = 'omero_root_password'
 
+# The 'web' recipes use this to determine whether the Omero web service
+# is enabled / started or disabled / stopped by the recipe.
+node.default['omero']['web_enabled'] = true
+
 node.normal['java']['install_flavor'] = 'openjdk'
 node.normal['java']['jdk_version'] = '7'
 node.normal['java']['accept_license_agreement'] = true
---
+++ recipes/web-internal.rb
@@ -0,0 +1,62 @@
+#
+# Cookbook Name:: omero
+# Recipe:: web-internal
+#
+# Copyright (c) 2014, The University of Queensland
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# * Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+# notice, this list of conditions and the following disclaimer in the
+# documentation and/or other materials provided with the distribution.
+# * Neither the name of the The University of Queensland nor the
+# names of its contributors may be used to endorse or promote products
+# derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE UNIVERSITY OF QUEENSLAND BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+include_recipe 'omero::server'
+
+omero_install = node['omero']['install']
+omero_user = node['omero']['user']
+omero_var = node['omero']['var']
+omero_web = node['omero']['web_enabled'] 
+
+directory "#{omero_var}/data" do
+  user omero_user
+  mode 0750
+end
+
+bash 'omero-web-internal-configuration' do
+  cwd "#{omero_install}/OMERO.server"
+  user omero_user
+  code <<-EOH
+  bin/omero config set omero.web.application_server development
+  bin/omero config set omero.web.debug 
+  EOH
+end
+
+template '/etc/init.d/omero-web' do
+  source "omero-init-web.erb"
+  mode 0755
+  variables({
+     :omero_user => omero_user,
+     :omero_home => "#{omero_install}/OMERO.server"
+  })            
+end
+
+service 'omero' do
+  action if omero_web then [ :enable, :start ] else [ :disable, :stop ] end
+end
