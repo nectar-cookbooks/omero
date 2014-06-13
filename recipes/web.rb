973e5f02c5712498ffcc390061c7443eaa7eec9c
Stephen Crawley <s.crawley@uq.edu.au>
2014-06-13 13:58:50 +1000

    Tweak ...

--- recipes/web.rb
+++ recipes/web.rb
@@ -47,7 +47,7 @@ bash 'omero-web-configuration' do
   code <<-EOH
   bin/omero config set omero.web.application_server "fastcgi"
   bin/omero config set omero.web.debug False
-  bin/omero web config nginx > /tmp/omero-nginx
+  bin/omero web config nginx --system > /tmp/omero-nginx
   EOH
 end
 
