e163c0b68c3d3af139b60aca200f3c744be91f21
Stephen Crawley <s.crawley@uq.edu.au>
2014-06-27 13:36:47 +1000

    Add default attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,7 +10,20 @@ node.default['omero']['root_password'] = 'omero_root_password'
 
 # The 'web' recipes use this to determine whether the Omero web service
 # is enabled / started or disabled / stopped by the recipe.
-node.default['omero']['web_enabled'] = true
+node.default['omero']['web']['enabled'] = true
+
+# The choices will be nginx or apache
+node.default['omero']['web']['frontend'] = 'nginx'
+
+node.default['omero']['web']['http_port'] = 80
+
+# The recipe used to install and configure the frontend webserver.  If unset,
+# the 'web.rb' recipe intuits one based on the 'frontend' attribute.
+node.default['omero']['web']['recipe'] = ''
+
+# If this is true, we attempt to inject suitable reverse proxy configs into
+# the frontend webserver configurations.
+node.default['omero']['web']['configure'] = true
 
 node.normal['java']['install_flavor'] = 'openjdk'
 node.normal['java']['jdk_version'] = '7'
